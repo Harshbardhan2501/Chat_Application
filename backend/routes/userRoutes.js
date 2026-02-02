5105d31a00e058dad10638fc9e5e8910132ee2d8
Harshbardhan <73756492+Harshbardhan2501@users.noreply.github.com>
2026-02-02 11:31:30 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchUsers } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/search', protect, searchUsers);
-
-module.exports = router;
